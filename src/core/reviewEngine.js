6e655b4b7036026af870c18c506cf8f52a02b204
yoonakorean <yoona.co.kr@gmail.com>
2026-07-22 02:22:51 +0800

    Implement initial ReviewEngine with placeholder methods

---
+++ src/core/reviewEngine.js
@@ -0,0 +1,12 @@
+export const ReviewEngine = {
+    async buildQueue(userId) {
+        // TODO: [Phase 2] 根據 SRS 演算法建立今日複習佇列
+        return [];
+    },
+    async finishReview(sessionResult) {
+        // TODO: [Phase 2] 結算複習階段
+    },
+    async saveReview(userId, wordPerformance) {
+        // TODO: [Phase 2] 更新單字的記憶曲線/熟練度
+    }
+};
